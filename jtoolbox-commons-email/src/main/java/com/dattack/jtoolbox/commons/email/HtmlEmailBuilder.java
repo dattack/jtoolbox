51087852a9393fcba4a51ff5c4d0e4c02d027b92
Carlos Varela <cvarela@dattack.com>
2022-03-01 13:16:00 +0100

    Add missing javadoc

--- jtoolbox-commons-email/src/main/java/com/dattack/jtoolbox/commons/email/HtmlEmailBuilder.java
+++ jtoolbox-commons-email/src/main/java/com/dattack/jtoolbox/commons/email/HtmlEmailBuilder.java
@@ -35,7 +35,7 @@ import static com.dattack.jtoolbox.util.function.FunctionHelper.throwingConsumer
  * @author cvarela
  * @since 0.1
  */
-@SuppressWarnings({"PMD.TooManyMethods", "unused"})
+@SuppressWarnings({ "PMD.TooManyMethods", "unused" })
 public class HtmlEmailBuilder {
 
     private final transient List<InternetAddress> toList;
@@ -92,78 +92,177 @@ public class HtmlEmailBuilder {
         return email;
     }
 
+    /**
+     * Adds a new BCC address.
+     *
+     * @param address the address in RFC822 format
+     * @return itself
+     * @throws AddressException if the parse of the address failed
+     */
     public HtmlEmailBuilder withBccAddress(final String address) throws AddressException {
         return withBccAddress(new InternetAddress(address));
     }
 
+    /**
+     * Adds a new BCC address.
+     *
+     * @param address the address to be added
+     * @return itself
+     */
     public HtmlEmailBuilder withBccAddress(final InternetAddress address) {
         this.bccList.add(address);
         return this;
     }
 
+    /**
+     * Adds a new CC address.
+     *
+     * @param address the address in RFC822 format
+     * @return itself
+     * @throws AddressException if the parse of the address failed
+     */
     public HtmlEmailBuilder withCcAddress(final String address) throws AddressException {
         return withCcAddress(new InternetAddress(address));
     }
 
+    /**
+     * Adds a new BCC address.
+     *
+     * @param address the address to be added
+     * @return itself
+     */
     public HtmlEmailBuilder withCcAddress(final InternetAddress address) {
         this.ccList.add(address);
         return this;
     }
 
+    /**
+     * Sets the FROM text.
+     *
+     * @param value the FROM value
+     * @return itself
+     */
     public HtmlEmailBuilder withFrom(final String value) {
         this.from = StringUtils.trimToNull(value);
         return this;
     }
 
+    /**
+     * Sets the hostname of the mail server.
+     *
+     * @param value the hostname of the mail server
+     * @return itself
+     */
     public HtmlEmailBuilder withHostName(final String value) {
         this.hostname = StringUtils.trimToNull(value);
         return this;
     }
 
+    /**
+     * Sets the text of the body.
+     *
+     * @param value the text of the body
+     * @return itself
+     */
     public HtmlEmailBuilder withMessage(final String value) {
         this.message = StringUtils.trimToEmpty(value);
         return this;
     }
 
+    /**
+     * Sets the password to connect to the mail server.
+     *
+     * @param value the plain password
+     * @return itself
+     */
     public HtmlEmailBuilder withPassword(final String value) {
         this.password = StringUtils.trimToNull(value);
         return this;
     }
 
+    /**
+     * Sets the port used to connect to the mail server.
+     *
+     * @param value the port to connect to the mail server
+     * @return itself
+     */
     public HtmlEmailBuilder withPort(final Integer value) {
         this.port = value;
         return this;
     }
 
+    /**
+     * Sets whether SSL/TLS encryption should be enabled for the SMTP transport upon connection (SMTPS/POPS).
+     *
+     * @param value the SSL/TLS encryption configuration
+     * @return itself
+     */
     public HtmlEmailBuilder withSslOnConnect(final Boolean value) {
         this.sslOnConnect = value;
         return this;
     }
 
+    /**
+     * Set or disable the STARTTLS encryption.
+     *
+     * @param value the STARTTLS configuration
+     * @return itself
+     */
     public HtmlEmailBuilder withStartTlsEnabled(final Boolean value) {
         this.startTlsEnabled = value;
         return this;
     }
 
+    /**
+     * Set or disable the required STARTTLS encryption.
+     *
+     * @param value the STARTTLS configuration
+     * @return itself
+     */
     public HtmlEmailBuilder withStartTlsRequired(final Boolean value) {
         this.startTlsRequired = value;
         return this;
     }
 
+    /**
+     * Sets the SUBJECT text.
+     *
+     * @param value the subject text
+     * @return itself
+     */
     public HtmlEmailBuilder withSubject(final String value) {
         this.subject = StringUtils.trimToEmpty(value);
         return this;
     }
 
+    /**
+     * Adds a new TO address.
+     *
+     * @param address the address in RFC822 format
+     * @return itself
+     * @throws AddressException if the parse of the address failed
+     */
     public HtmlEmailBuilder withToAddress(final String address) throws AddressException {
         return withToAddress(new InternetAddress(address));
     }
 
+    /**
+     * Adds a new TO address.
+     *
+     * @param address the address to be added
+     * @return itself
+     */
     public HtmlEmailBuilder withToAddress(final InternetAddress address) {
         this.toList.add(address);
         return this;
     }
 
+    /**
+     * Sets the username to connect to the mail server.
+     *
+     * @param value the username to connect to the mail server
+     * @return itself
+     */
     public HtmlEmailBuilder withUsername(final String value) {
         this.username = StringUtils.trimToNull(value);
         return this;
